bbe181ec14035822b46edac31da8c68976b85d45
Gareth Lowrie <gareth@lowrie.io>
2018-07-22 22:09:33 +0100

    Slight amends.
    Added key prop to mapped messages.
    Fixed issue with z-index preventing user from typing in.

--- src/Chat/presentational.js
+++ src/Chat/presentational.js
@@ -18,17 +18,16 @@ const Chat = ({
   const bodyClassName = classNames("chat-history", {
     centerNoMessages: messages.length === 0
   });
-
   return (
-    <div className={classNames("live-chat", className)}>
+    <div className={classNames("liveChat", className)}>
       <ChatHeader onClose={onClose}>{header}</ChatHeader>
 
       <div className="chat">
         <div className={bodyClassName}>
           {messages &&
             messages.length > 0 &&
-            messages.map(({ message, time, name }) => (
-              <div>
+            messages.map(({ message, time, name }, index) => (
+              <div key={index}>
                 <ChatMessage name={name} time={time}>
                   {message}
                 </ChatMessage>
